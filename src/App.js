95bbdba3f7f1ed97d4e9a827e5d648369d93e2fc
Nasa-ee <nasaee.dev@gmail.com>
2023-03-04 23:13:51 +0700

    finish Case Studies section

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React from 'react';
 
-import { Brand, CTA, Navbar } from './components';
-import {Blog,Features,Footer,Header,Possibility,WhatGPT3} from './containers'; // prettier-ignore
+import {
+  Footer,
+  Blog,
+  Possibility,
+  Features,
+  WhatGPT3,
+  Header,
+} from './containers';
+import { CTA, Brand, Navbar } from './components';
+
 import './App.css';
 
-const App = () => {
-  return (
-    <div className="App">
-      <div className="gradient__bg">
-        <Navbar />
-        <Header />
-      </div>
-      <Brand />
-      <WhatGPT3 />
-      <Features />
-      <Possibility />
-      <CTA />
-      <Blog />
-      <Footer />
+const App = () => (
+  <div className="App">
+    <div className="gradient__bg">
+      <Navbar />
+      <Header />
     </div>
-  );
-};
+    <Brand />
+    <WhatGPT3 />
+    <Features />
+    <Possibility />
+    <CTA />
+    <Blog />
+    <Footer />
+  </div>
+);
 
 export default App;
